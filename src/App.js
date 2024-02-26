350762bdcdab5ff60578af8994c00367107b50c2
ThapeloM01 <sinahthapelo01@gmail.com>
2024-02-26 19:30:38 +0200

    Added the github link

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import Weather from "./Weather";
 
 export default function App (){
    return (
-    <div className="App">
-      <div className="container">
-    <weather defaultCity="Lisbon"/>
-    <footer>
-      This project was coded by {""}
-      <a href="" target="blank"> Mokoena Thapelo</a> {""}
-      and is {""} <a href="">open-sourced on GitHub</a>
-    </footer>
-    </div>
-    </div>
-  )
+     <div className="App">
+       <div className="container">
+         <weather defaultCity="Lisbon" />
+         <footer>
+           This project was coded by {""}
+           <a href="" target="blank">
+             {" "}
+             Mokoena Thapelo
+           </a>{" "}
+           {""}
+           and is {""}{" "}
+           <a href="https://github.com/ThapeloM01/weather-react">
+             open-sourced on GitHub
+           </a>
+         </footer>
+       </div>
+     </div>
+   );
 }
   
